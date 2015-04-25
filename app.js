4901e4732e62979353e4525be71429c397b77615
hcavalle <harrison.cavallero@gmail.com>
2015-04-25 16:52:22 -0700

    runs import on startup

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 var Hapi = require('hapi');
 var config = require('./appconf.js');
 var models = require('./models');
-console.log(config);
+var _import = require('./import/import.js');
 
 var server = new Hapi.Server();
 server.connection(config.application);
 
 server.route(require('./lib/routes'));
 
-models.sequelize.sync({force: true}).then(function() {
+models.sequelize.sync().then(function() {
   server.start(function () {
     console.log('running on '+config.application.port);
   });
+  _import.import_json();
 }).catch(function(error) {
   console.log(error);
 });
--- import/import.js
+++ import/import.js
@@ -1,26 +1,33 @@
-var models = require('../models');
-var fs        = require("fs");
-var path      = require("path");
-var Sequelize = require("sequelize");
-var basename  = path.basename(module.filename);
-
-fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf(".") !== 0) && (file !== basename);
-  })
-  .forEach(function(file) {
-    var json = JSON.parse(fs.readFileSync(file));
-    for (var item in json) {
-      models.ImpactModel.create({
-        name: json[item].Model,
-        definition: json[item].Definition,
-        brands: json[item]['Sample Brands'],
-        category: json[item].Categories,
-        type: file
-      }).then(function(imodel){
-        console.log('created model: ');
+module.exports =  {
+  import_json: function () {
+    console.log('import called');
+    var models = require('../models');
+    var fs        = require("fs");
+    var path      = require("path");
+    var Sequelize = require("sequelize");
+    var basename  = path.basename(module.filename);
+    console.log(__dirname);
+    fs
+      .readdirSync(__dirname)
+      .filter(function(file) {
+        return (file.indexOf(".") !== 0) && (file !== basename);
+      })
+      .forEach(function(file) {
+        var json = JSON.parse(fs.readFileSync(__dirname+'/'+file));
+        console.log('json: ');
+        console.log(json);
+        for (var item in json) {
+          models.ImpactModel.create({
+            name: json[item].Model,
+            definition: json[item].Definition,
+            brands: json[item]['Sample Brands'],
+            category: json[item].Categories,
+            type: file
+          }).then(function(imodel){
+            console.log('created model: ');
+          });
+        }
       });
-    }
-  });
+  }
+};
 
